9c831e198ba326f2aa5ee4709c231f369b700082
domi04151309 <dominikworldlp@gmail.com>
2023-08-24 19:07:59 +0200

    Make history saving optional

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-import { HISTORY_FILE, PORT } from './src/config.js';
+import { HISTORY_FILE, PERSISTENT_HISTORY, PORT } from './src/config.js';
 import { existsSync, readFileSync, writeFileSync } from 'fs';
 import express from 'express';
 import { getData } from './src/fetcher.js';
@@ -7,17 +7,28 @@ import open from 'open';
 const historyData = [];
 const app = express();
 
-try {
-  if (existsSync(HISTORY_FILE)) historyData.push(
-    ...JSON.parse(readFileSync(HISTORY_FILE))
-  );
-} catch (exception) {
-  console.error('Failed opening history file!', exception);
+function exitHandler(options) {
+  if (options.cleanup) writeFileSync(HISTORY_FILE, JSON.stringify(historyData));
+  if (options.exit) process.exit();
 }
 
 async function fetchNewData() {
   while (historyData.length > 8_640) historyData.shift();
   historyData.push(await getData());
+}
+
+if (PERSISTENT_HISTORY) {
+  try {
+    if (existsSync(HISTORY_FILE)) historyData.push(
+      ...JSON.parse(readFileSync(HISTORY_FILE))
+    );
+  } catch (exception) {
+    console.error('Failed opening history file!', exception);
+  }
+  process.on('exit', exitHandler.bind(null, { cleanup: true }));
+  process.on('SIGINT', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
+  process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
 }
 
 await fetchNewData();
@@ -41,13 +52,3 @@ app.listen(PORT, () => {
   console.log(url);
   open(url);
 });
-
-function exitHandler(options) {
-  if (options.cleanup) writeFileSync(HISTORY_FILE, JSON.stringify(historyData));
-  if (options.exit) process.exit();
-}
-
-process.on('exit', exitHandler.bind(null, { cleanup: true }));
-process.on('SIGINT', exitHandler.bind(null, { exit: true }));
-process.on('SIGUSR1', exitHandler.bind(null, { exit: true }));
-process.on('SIGUSR2', exitHandler.bind(null, { exit: true }));
--- src/config.js
+++ src/config.js
@@ -1,3 +1,4 @@
 export const HISTORY_FILE = 'historyData.json';
+export const PERSISTENT_HISTORY = false;
 export const PLANT_IP_ADDRESS = '192.168.20.60';
 export const PORT = 3000;
