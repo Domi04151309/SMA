2da9af305d9c28c07b111b76578645373f4b7bdd
domi04151309 <dominikworldlp@gmail.com>
2023-09-04 02:10:55 +0200

    Use different package for subnet scanning

--- src/discovery.js
+++ src/discovery.js
@@ -1,19 +1,19 @@
 process.env.NODE_TLS_REJECT_UNAUTHORIZED = '0';
-import netmask from 'netmask';
+import ip from 'ip';
 import { networkInterfaces } from 'node:os';
 
 /**
  *
- * @param {string} ip
+ * @param {string} ipAddress
  * @returns {Promise<string|null>}
  */
-async function isInverter(ip) {
+async function isInverter(ipAddress) {
   try {
     const response = await fetch(
-      'https://' + ip + '/dyn/getDashTime.json'
+      'https://' + ipAddress + '/dyn/getDashTime.json'
     ).catch(() => null);
     if (response === null || response.status !== 200) return null;
-    return ip;
+    return ipAddress;
   } catch {
     return null;
   }
@@ -25,7 +25,9 @@ async function isInverter(ip) {
 export async function findInverters() {
   const localInterface = Object.values(networkInterfaces())
     .flat()
-    .find(item => item?.internal === false && item?.family === 'IPv4');
+    .find(
+      item => item?.internal === false && ip.isPrivate(item?.address)
+    );
 
   if (!localInterface || !localInterface.cidr) throw new Error(
     'Unknown network configuration'
@@ -33,11 +35,12 @@ export async function findInverters() {
 
   /** @type {Promise<string|null>[]} */
   const promises = [];
-
-  // eslint-disable-next-line unicorn/no-array-for-each
-  new netmask.Netmask(localInterface.cidr).forEach(ip => {
-    promises.push(isInverter(ip));
-  });
+  const subnet = ip.cidrSubnet(localInterface.cidr);
+  const firstAddress = ip.toLong(subnet.firstAddress);
+  const lastAddress = ip.toLong(subnet.lastAddress);
+  for (
+    let ipAddress = firstAddress; ipAddress <= lastAddress; ipAddress++
+  ) promises.push(isInverter(ip.fromLong(ipAddress)));
 
   const results = await Promise.all(promises);
   return /** @type {string[]} */ (results.filter(item => item !== null));
